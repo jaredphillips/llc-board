8bf9e93942564154fd8feb82580cbc9b55f3ee94
Jared Phillips <jaredtphillips@me.com>
2013-12-11 20:59:33 -0500

    fixin' for heroku, postgres does not allow for strings longer than 255. Needed to make them :text

--- db/migrate/20131209014226_add_job_description_to_postings.rb
+++ db/migrate/20131209014226_add_job_description_to_postings.rb
@@ -1,5 +1,5 @@
 class AddJobDescriptionToPostings < ActiveRecord::Migration
   def change
-    add_column :postings, :job_description, :string
+    add_column :postings, :job_description, :text, limit: nil
   end
 end
--- db/migrate/20131210051031_add_how_to_apply_to_posting.rb
+++ db/migrate/20131210051031_add_how_to_apply_to_posting.rb
@@ -1,5 +1,5 @@
 class AddHowToApplyToPosting < ActiveRecord::Migration
   def change
-    add_column :postings, :how_to_apply, :string
+    add_column :postings, :how_to_apply, :text, limit: nil
   end
 end
